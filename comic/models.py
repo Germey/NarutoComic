d229b79802cf1059860f75849d694ad1642acee9
cqc <1016903103@qq.com>
2016-02-13 21:44:21 +0800

    make models

--- comic/models.py
+++ comic/models.py
@@ -1,17 +1,16 @@
+# coding:utf-8
 from __future__ import unicode_literals
 
-from django.db import models
-
 # Create your models here.
-# coding:utf-8
 from django.db import models
 
 
-class Article(models.Model):
-    title = models.CharField(u'title', max_length=256)
-    content = models.TextField(u'content')
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
 
-    pub_date = models.DateTimeField(u'pub_date', auto_now_add=True, editable = True)
-    update_time = models.DateTimeField(u'update_time',auto_now=True, null=True)
-    def __unicode__(self):
-        return self.title
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    page = models.IntegerField(max_length=5)
