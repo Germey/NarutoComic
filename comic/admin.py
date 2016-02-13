320ec01bc478c66b69f2b7b299022598912b0c34
cqc <1016903103@qq.com>
2016-02-13 22:13:59 +0800

    registaer model

--- comic/admin.py
+++ comic/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Content, Image
+
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'page')
+
+
+admin.site.register(Content, ContentAdmin)
+admin.site.register(Image, ImageAdmin)
